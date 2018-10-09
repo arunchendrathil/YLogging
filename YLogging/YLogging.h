debdce8888354ceacf4a655d99aa479ac8010eaf
Ashik Hashim <support@tobeprecise.com>
2018-10-09 11:36:36 +0400

    Initial Commit

---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by APPLE on 7/17/1397 AP.
+//  Copyright © 1397 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+

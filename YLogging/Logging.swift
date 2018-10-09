75302f521d34301e207e38ed1533879b85afc065
APPLE <apple@APPLEs-iMac.local>
2018-10-09 12:22:39 +0400

    Initial commit

--- YLogging/Logging.swift
+++ YLogging/Logging.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+public class YLog
+{
+    
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+    
+}
